3ecb4b37beefe2b9a109ca5a17b1cfec05b93b2b
Leroy Medeiros <lelo.drax@gmail.com>
2019-10-08 12:10:07 -0300

    feat: image upload

---
+++ frontend/components/CreateItem.js
@@ -0,0 +1,149 @@
+import React, { Component } from 'react'
+import { Mutation } from 'react-apollo'
+import gql from 'graphql-tag'
+import Router from 'next/router'
+import Form from './styles/Form'
+import formatMoney from '../lib/formatMoney'
+import Error from './ErrorMessage'
+
+const CREATE_ITEM_MUTATION = gql`
+  mutation CREATE_ITEM_MUTATION(
+    $title: String!
+    $description: String!
+    $price: Int!
+    $image: String
+    $largeImage: String
+  ) {
+    createItem(
+      title: $title
+      description: $description
+      price: $price
+      image: $largeImage
+      largeImage: $image
+    ) {
+      id
+    }
+  }
+`
+
+class CreateItem extends Component {
+  state = {
+    title: '',
+    description: '',
+    image: '',
+    largeImage: '',
+    price: 0,
+  }
+
+  handleChange = event => {
+    event.preventDefault()
+    const { name, type, value } = event.target
+    const parsedValue = type === 'number' ? parseFloat(value) : value
+    this.setState({ [name]: parsedValue })
+  }
+
+  uploadFile = async event => {
+    const files = event.target.files
+    const data = new FormData()
+    data.append('file', files[0])
+    data.append('upload_preset', 'sickfits')
+
+    const res = await fetch(
+      'https://api.cloudinary.com/v1_1/leroybm/image/upload',
+      {
+        method: 'POST',
+        body: data,
+      },
+    )
+    const file = await res.json()
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url,
+    })
+  }
+
+  render() {
+    return (
+      <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
+        {(createItem, { loading, error }) => (
+          <Form
+            onSubmit={async event => {
+              event.preventDefault()
+              const res = await createItem()
+              Router.push({
+                pathname: '/item',
+                query: { id: res.data.createItem.id },
+              })
+            }}
+          >
+            <Error error={error} />
+            <fieldset disabled={loading} aria-busy={loading}>
+              <label htmlFor="file">
+                Image
+                <input
+                  type="file"
+                  name="file"
+                  id="file"
+                  placeholder="Upload an image"
+                  required
+                  onChange={this.uploadFile}
+                />
+                {this.state.image && (
+                  <img
+                    src={this.state.image}
+                    width="200"
+                    alt="Upload Preview"
+                  ></img>
+                )}
+              </label>
+
+              <label htmlFor="title">
+                Title
+                <input
+                  type="text"
+                  name="title"
+                  id="title"
+                  placeholder="Title"
+                  required
+                  value={this.state.title}
+                  onChange={this.handleChange}
+                />
+              </label>
+
+              <label htmlFor="price">
+                Price
+                <input
+                  type="number"
+                  name="price"
+                  id="price"
+                  placeholder="Price"
+                  required
+                  value={this.state.price}
+                  onChange={this.handleChange}
+                />
+              </label>
+
+              <label htmlFor="description">
+                Description
+                <textarea
+                  type="text"
+                  name="description"
+                  id="description"
+                  placeholder="Enter A Description"
+                  required
+                  value={this.state.description}
+                  onChange={this.handleChange}
+                />
+              </label>
+
+              <button type="submit">Submit</button>
+            </fieldset>
+          </Form>
+        )}
+      </Mutation>
+    )
+  }
+}
+
+export default CreateItem
+export { CREATE_ITEM_MUTATION }
