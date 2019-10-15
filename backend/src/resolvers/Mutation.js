a6b14274349ed020991cb1515cd1d422dcaaa143
Leroy Medeiros <lelo.drax@gmail.com>
2019-10-15 11:38:05 -0300

    fix: login refactor

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,7 +1,15 @@
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
+const { errorIfFalse } = require('../utils/error')
+const loginUtils = require('../utils/login')
+const { HOUR } = loginUtils
 
 const Mutation = {
+  /**
+   * Creates item in store
+   * @param {*} parent
+   * @param {*} args
+   * @param {*} ctx
+   * @param {*} info
+   */
   async createItem(parent, args, ctx, info) {
     // TODO: Check if they are logged in
 
@@ -17,6 +25,13 @@ const Mutation = {
     return item
   },
 
+  /**
+   * Updates store item
+   * @param {*} parent
+   * @param {*} args
+   * @param {*} ctx
+   * @param {*} info
+   */
   updateItem(parent, args, ctx, info) {
     const updates = { ...args }
     delete updates.id
@@ -31,6 +46,13 @@ const Mutation = {
     )
   },
 
+  /**
+   * Deletes item from store
+   * @param {*} parent
+   * @param {*} args
+   * @param {*} ctx
+   * @param {*} info
+   */
   async deleteItem(parent, args, ctx, info) {
     const where = { id: args.id }
     const item = await ctx.db.query.item({ where }, `{ id title }`)
@@ -38,9 +60,16 @@ const Mutation = {
     return ctx.db.mutation.deleteItem({ where }, info)
   },
 
+  /**
+   * Creates user account
+   * @param {*} parent
+   * @param {*} args
+   * @param {*} ctx
+   * @param {*} info
+   */
   async signup(parent, args, ctx, info) {
     args.email = args.email.toLowerCase()
-    const password = await bcrypt.hash(args.password, 10)
+    const password = await loginUtils.hashPassword(args.password)
     const user = await ctx.db.mutation.createUser(
       {
         data: {
@@ -51,34 +80,89 @@ const Mutation = {
       },
       info,
     )
-    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET)
-    ctx.response.cookie('token', token, {
-      httpOnly: true,
-      maxAge: 1000 * 60 * 60 * 24 * 365,
-    })
+    loginUtils.setLoginCookie(user.id, ctx)
     return user
   },
 
-  async signin(parent, { email, password }, ctx, info) {
+  /**
+   * Signs user in
+   * @param {*} parent
+   * @param {*} args
+   * @param {*} ctx
+   * @param {*} info
+   */
+  async signin(parent, args, ctx, info) {
+    const { email, password } = args
     const user = await ctx.db.query.user({ where: { email } })
-    if (!user) {
-      throw new Error(`No such user found for email ${email}`)
-    }
-    const valid = await bcrypt.compare(password, user.password)
-    if (!valid) {
-      throw new Error('Invalid password!')
-    }
-    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET)
-    ctx.response.cookie('token', token, {
-      httpOnly: true,
-      maxAge: 1000 * 60 * 60 * 24 * 365,
-    })
+    errorIfFalse(!user, `No such user found for email ${email}`)
+    const valid = await loginUtils.comparePasswords(password, user.password)
+    errorIfFalse(!valid, 'Invalid password!')
+    loginUtils.setLoginCookie(user.id, ctx)
     return user
   },
 
+  /**
+   * Signs user out
+   * @param {*} parent
+   * @param {*} args
+   * @param {*} ctx
+   * @param {*} info
+   */
   signout(parent, args, ctx, info) {
     ctx.response.clearCookie('token')
-    return { message: 'Goodbye!' }
+    return { message: 'Success!' }
+  },
+
+  /**
+   * Request reset token
+   * @param {*} parent
+   * @param {*} args
+   * @param {*} ctx
+   * @param {*} info
+   */
+  async requestReset(parent, args, ctx, info) {
+    const { email } = args
+    const user = await ctx.db.query.user({ where: { email } })
+    errorIfFalse(!user, `No such user found for email ${email}`)
+    const resetToken = await loginUtils.generateResetToken()
+    const resetTokenExpiry = Date.now() + HOUR
+    const res = await ctx.db.mutation.updateUser({
+      where: { email: email },
+      data: { resetToken, resetTokenExpiry },
+    })
+    return { message: 'Success' }
+  },
+
+  /**
+   * Verifies password and resetToken, then resets user password
+   * @param {*} parent
+   * @param {*} args
+   * @param {*} ctx
+   * @param {*} info
+   */
+  async resetPassword(parent, args, ctx, info) {
+    const { password, confirmPassword, resetToken } = args
+    errorIfFalse(password !== confirmPassword, `Passwords don't match`)
+    const [user] = await ctx.db.query.users({
+      where: { resetToken, resetTokenExpiry_gte: Date.now() - HOUR },
+    })
+    errorIfFalse(!user, 'Invalid reset request, please try again.')
+    const newPassword = await loginUtils.hashPassword(password)
+    const updatedUser = await ctx.db.mutation.updateUser(
+      {
+        data: {
+          password: newPassword,
+          resetToken: null,
+          resetTokenExpiry: null,
+        },
+        where: {
+          id: user.id,
+        },
+      },
+      info,
+    )
+    setLoginCookie(updatedUser.id, ctx)
+    return updatedUser
   },
 }
 
