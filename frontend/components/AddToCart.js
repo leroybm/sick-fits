8ddad80f640060d7d214a60a2f7d9ce65bb1fa9d
Leroy Medeiros <lelo.drax@gmail.com>
2019-10-29 14:52:35 -0300

    feat: cart display

--- frontend/components/AddToCart.js
+++ frontend/components/AddToCart.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Mutation } from 'react-apollo'
 import gql from 'graphql-tag'
 import PropTypes from 'prop-types'
+import { CURRENT_USER_QUERY } from './User'
 
 const ADD_TO_CART_MUTATION = gql`
   mutation addToCart($id: ID!) {
@@ -20,8 +21,16 @@ class AddToCart extends Component {
   render() {
     const { id } = this.props
     return (
-      <Mutation mutation={ADD_TO_CART_MUTATION} variables={{ id }}>
-        {addToCart => <button onClick={addToCart}>Add To Cart</button>}
+      <Mutation
+        mutation={ADD_TO_CART_MUTATION}
+        variables={{ id }}
+        refetchQueries={[{ query: CURRENT_USER_QUERY }]}
+      >
+        {(addToCart, { loading }) => (
+          <button onClick={addToCart} disabled={loading}>
+            Add{loading && 'ing'} To Cart
+          </button>
+        )}
       </Mutation>
     )
   }
